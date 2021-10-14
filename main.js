ab8a3f637ac3643c71002ba02d360d45f0850e9c
sirisha-busani <72750215+sirisha-busani@users.noreply.github.com>
2021-10-15 04:54:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function showImage(){
+    document.getElementById("diwaliImg").style.display = "inline";
+    document.getElementById("heading").innerHTML = "Festivals Of India"
+}
+
+function hideImage(){
+    document.getElementById("diwaliImg").style.display = "none";
+}
